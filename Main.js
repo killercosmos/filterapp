cf0947f07c2ed5dbb60b3cbe01e21660448bf866
killercosmos <76935007+killercosmos@users.noreply.github.com>
2021-11-12 19:09:09 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,34 @@
+var noseX = 0;
+var noseY = 0;
+function preload() {
+clown_nose = loadImage("https://i.postimg.cc/bv207Jhb/pngwing-com.png");
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotposes);
+}
+function modelLoaded() {
+    console.log("poseNet is initializes");
+}
+function gotposes(results) {
+  if(results.length > 0){
+      console.log(results);
+      noseX = results[0].pose.nose.x-15;
+      noseY = results[0].pose.nose.y-9;
+  }
+}
+function draw() {
+    
+image(video, 0,0,300,300);
+image(clown_nose, noseX, noseY, 50, 50);
+}
+function take_snapshot() {
+    save("Shocked_pikachu_face.jpg");
+}
